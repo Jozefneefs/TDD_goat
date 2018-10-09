4173e30da1118a5fa2d6f61a3c0cdd643a166420
Your Name <you@example.com>
2018-10-09 23:20:36 +0200

    Pag 64, gefoefeld met de tabel, maar er is altijd nog maar één lijn

--- lists/views.py
+++ lists/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 def home_page(request):
     print(request.POST)
+    print ("pap")
+    print(request)
     return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', "")})
 
 
