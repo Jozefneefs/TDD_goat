9559d6ca32657ec3fccc0eb0c8ce2e0e4c11334c
Your Name <you@example.com>
2018-10-20 00:46:07 +0200

    bladzijde 80 database begint te werken.

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 
 def home_page(request):
-    print(request.POST)
-    print ("pap")
-    print(request)
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', "")})
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+#   Item.objects.first().delete()
 
+    item_list = Item.objects.all()
+    return render(request, 'home.html', {'items': item_list})
 
 # Create your views here.
