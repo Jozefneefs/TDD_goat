a3411d5fc0e95edb03a3c61a010b70732f3dd45c
Your Name <you@example.com>
2018-10-19 21:48:44 +0200

    p.70 database opgezet

--- functional_tests.py
+++ functional_tests.py
@@ -37,7 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 
@@ -48,7 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
