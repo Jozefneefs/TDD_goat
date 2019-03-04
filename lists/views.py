22ea8f82fdfd28c96ab918e0dfe0d858cc26c895
Your Name <you@example.com>
2019-03-04 23:24:30 +0100

    Bladzijde 105 halfweg.

--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,14 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 #   Item.objects.first().delete()
 
     item_list = Item.objects.all()
     return render(request, 'home.html', {'items': item_list})
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
 
 # Create your views here.
