45b943b1d6cf440652df776e4f8df8c87a0d9064
Your Name <you@example.com>
2019-11-29 00:37:43 +0100

    einde pagina 122

--- lists/views.py
+++ lists/views.py
@@ -7,8 +7,9 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'items': items})
 
 
@@ -16,5 +17,5 @@ def view_list(request):
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_.id}/')
 
