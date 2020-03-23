4a6e9c0a6ecf26e0a6c1b9842da4de8c706d8fd4
Your Name <you@example.com>
2020-03-23 23:42:09 +0100

    A final Refactor Using URL includes einde pagina 122

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -113,4 +113,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+
+
         # Satisfied, they both go back to sleep
