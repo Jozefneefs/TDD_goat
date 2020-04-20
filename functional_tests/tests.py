0f3500aeae58f2c2431dc2c6f3a6df92aacd4dc6
Your Name <you@example.com>
2020-04-20 23:44:09 +0200

    https://www.obeythetestinggoat.com/book/chapter_manual_deployment.html (9)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -88,6 +88,7 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
+        #mageweg
 
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
